d2f7ca96ff247552aad1ada9521c6f69a27491cc
Conor Sullivan <conorwsullivan@gmail.com>
2023-05-05 05:56:22 +0700

    Use async/await for sqlite

--- server.js
+++ server.js
@@ -3,36 +3,40 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const sqlite3 = require('sqlite3').verbose();
-const dbFile = './chatroom.db';
+const sqlite = require('sqlite');
+const dbFile = './world.db';
 
 app.use(express.static('public'));
 
-// Initialize SQLite database
-const db = new sqlite3.Database(dbFile, (err) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log('Connected to the SQLite database.');
-    db.run(
-      `CREATE TABLE IF NOT EXISTS messages (
+async function initializeDatabase() {
+  const db = await sqlite.open({
+    filename: dbFile,
+    driver: sqlite3.Database,
+  });
+
+  console.log('Connected to the SQLite database.');
+
+  try {
+    await db.run(`
+      CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         room TEXT NOT NULL,
         character TEXT NOT NULL,
         text TEXT NOT NULL,
         timestamp TEXT NOT NULL
-      );`,
-      (err) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log('Table created or already exists.');
-        }
-      }
-    );
-  }
-});
+      );
+    `);
 
-function send(message) {
+    console.log('Table created or already exists.');
+
+    return db;
+  } catch (err) {
+    console.error(err.message);
+    throw err;
+  }
+}
+
+async function send(db, message) {
   const messageData = {
     timestamp: new Date().toISOString(),
     ...message,
@@ -40,68 +44,64 @@ function send(message) {
 
   console.log(messageData);
 
-  // Insert the message into the database
-  db.run(
-    `INSERT INTO messages (room, character, text, timestamp) VALUES (?, ?, ?, ?);`,
-    [messageData.room, messageData.character, messageData.text, messageData.timestamp],
-    (err) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log('Message inserted into the database.');
+  try {
+    await db.run(
+      `INSERT INTO messages (room, character, text, timestamp) VALUES (?, ?, ?, ?);`,
+      [messageData.room, messageData.character, messageData.text, messageData.timestamp]
+    );
 
-        // Emit the message to all clients in the room
-        io.to(message.room).emit('message', messageData);
-      }
-    }
-  );
+    console.log('Message inserted into the database.');
+
+    io.to(message.room).emit('message', messageData);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
 app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+initializeDatabase().then((db) => {
+  io.on('connection', async (socket) => {
+    console.log('a user connected');
 
-  // Assign the user to the default room
-  const defaultRoom = 'general';
-  const defaultUsername = 'Anonymous';
-  socket.join(defaultRoom);
+    const defaultRoom = 'general';
+    const defaultUsername = 'Anonymous';
+    socket.join(defaultRoom);
 
-  socket.emit('reset');
-  socket.emit('room', defaultRoom);
-  socket.emit('character', defaultUsername);
+    socket.emit('reset');
+    socket.emit('room', defaultRoom);
+    socket.emit('character', defaultUsername);
 
-  // Get the last ~10 messages from the database and emit them
-  db.all(
-    `SELECT * FROM messages WHERE room = ? ORDER BY timestamp DESC LIMIT 10;`,
-    [defaultRoom],
-    (err, rows) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        socket.emit('previous messages', rows.reverse());
-      }
+    try {
+      const rows = await db.all(
+        `SELECT * FROM messages WHERE room = ? ORDER BY timestamp DESC LIMIT 10;`,
+        [defaultRoom]
+      );
+      socket.emit('previous messages', rows.reverse());
+    } catch (err) {
+      console.error(err.message);
     }
-  );
 
-  // Listen for new messages
-  socket.on('message', (msg) => {
-    const messageData = {
-      room: defaultRoom,
-      character: defaultUsername,
-      ...msg,
-    };
+    socket.on('message', (msg) => {
+      const messageData = {
+        room: defaultRoom,
+        character: defaultUsername,
+        ...msg,
+      };
 
-    send(messageData);
+      send(db, messageData);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
   });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  const PORT = process.env.PORT || 3000;
+  http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
   });
-});
-
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
+}).catch((err) => {
+  console.error(err);
 });
 
